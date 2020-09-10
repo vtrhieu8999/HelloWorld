946bf0fb0b9ab7f97a6f4607a2cc94be54d9b628
Vuong_Trung_Hieu <v.hieu1998@gmail.com>
2020-09-10 17:07:23 +0700

    Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name= sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
